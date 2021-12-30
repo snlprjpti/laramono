b0237ef9465a9fc75743524f9e94130c90de0cab
Sunil Prajapati <sunil.prajapati@hazesoft.co>
2021-12-30 18:53:46 +0545

    v4

--- src/autoload.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-require_once __DIR__ . '/Config.php';
-require_once __DIR__ . '/ConfigFactory.php';
-require_once __DIR__ . '/Exception/ConfigurationException.php';
-require_once __DIR__ . '/PublicAccessTokenResolver.php';
